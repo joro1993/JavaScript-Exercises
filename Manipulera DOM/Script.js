971eb556750f026e2a1b90355cfd5d7db18ddb59
Jonas Rosberg <rojo19@student.ju.se>
2020-03-07 12:16:19 +0100

    Manipulate DOM, done

---
+++ Manipulera DOM/Script.js
@@ -0,0 +1,8 @@
+var myBody = document.createElement("div");
+myBody.innerHTML = "<p> Jonas Rosberg </p>";
+document.body.appendChild(myBody);
+
+var myArray = ["Laga mat", " gå ut med min hund", " Lära mig programmera", " laga mat"];
+var div = document.createElement("div");
+div.innerHTML = myArray;
+document.body.appendChild(div);
