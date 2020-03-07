1bd413304c857a8453ef5221a9f78b6f85e2cc2c
Jonas Rosberg <rojo19@student.ju.se>
2020-03-07 12:32:30 +0100

    functions, done

---
+++ Functions/Script.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById("myBtn");
+var output = document.getElementById("myInput");
+
+btn.addEventListener("click", function(){
+    var inpt = document.getElementById("myInput").value;
+    var temp = inpt.toUpperCase();
+    output.value = temp;
+});
